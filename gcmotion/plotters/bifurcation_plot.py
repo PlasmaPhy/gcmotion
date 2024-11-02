6ead6461fc7fb65dad2f12924859b3026201d169
George <georgetzim23@gmail.com>
2024-11-02 11:29:53 +0200

    added_bifurcation

---
+++ gcmotion/plotters/bifurcation_plot.py
@@ -0,0 +1,68 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from gcmotion.scripts.bifurcation import bifurcation
+from gcmotion.classes.collection import Collection
+
+
+def bifurcation_plot(
+    collection: Collection,
+    theta_lim: list = [-np.pi, np.pi],
+    P_theta_lim: list = [0.01, 1.3],
+    iterations: int = 250,
+    info: bool = False,
+):
+
+    thetas_fixed, P_thetas_fixed, num_of_fp = bifurcation(
+        collection=collection,
+        theta_lim=theta_lim,
+        P_theta_lim=P_theta_lim,
+        iterations=iterations,
+        info=info,
+    )
+
+    fig, ax = plt.subplots(3, 1, figsize=(6, 4), sharex=True)
+    plt.xlabel(r"$P_{\zeta}$")
+
+    ax_theta = ax[0]
+    ax_P_theta = ax[1]
+    ax_ndfp = ax[2]
+
+    P_zeta_plot = []
+    theta_plot = []
+    P_theta_plot = []
+
+    particles = collection.particles
+
+    # Theta Bifurcation
+    for i, p in enumerate(particles):
+        P_zeta = p.Pzeta0
+        y_list = thetas_fixed[i]
+        P_zeta_plot.extend([P_zeta] * len(y_list))
+        theta_plot.extend(y_list)
+
+    # ax_theta.set_title("theta Bifurcation Diagram")
+    ax_theta.set_ylabel(r"$\theta_s$")
+    ax_theta.scatter(P_zeta_plot, theta_plot, s=2)
+
+    P_zeta_plot = []
+
+    # P_theta Bifurcation
+    for i, p in enumerate(particles):
+        P_zeta = p.Pzeta0
+        y_list = P_thetas_fixed[i]
+        P_zeta_plot.extend([P_zeta] * len(y_list))
+        P_theta_plot.extend(y_list)
+
+    # ax_P_theta.set_title(r"$P_{theta}$ Bifurcation Diagram")
+    ax_P_theta.set_ylabel(r"$P_{\theta_s}$")
+    ax_P_theta.scatter(P_zeta_plot, P_theta_plot, s=2)
+
+    # Number of distinct fixed points Diagram
+    # ax_ndfp.set_title("Number of Fixed Points Bifurcation Diagram")
+    P_zetas = [p.Pzeta0 for p in particles]
+    ax_ndfp.set_ylabel("Fixed Points")
+    ax_ndfp.scatter(P_zetas, num_of_fp, s=2)
+
+    plt.ion()
+    plt.show(block=True)
---
+++ gcmotion/scripts/bifurcation.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+from gcmotion.scripts.fixed_points import fixed_points
+from gcmotion.classes.collection import Collection
+
+
+def bifurcation(
+    collection: Collection,
+    theta_lim: list = [-np.pi, np.pi],
+    P_theta_lim: list = [0.01, 1.3],
+    iterations: int = 250,
+    info: bool = False,
+):
+
+    theta_min = theta_lim[0]
+    theta_max = theta_lim[1]
+
+    P_theta_min = P_theta_lim[0]
+    P_theta_max = P_theta_lim[1]
+
+    num_of_fp = []
+    fp = []
+
+    thetas_fixed = []
+    P_thetas_fixed = []
+
+    particles = collection.particles
+
+    p1 = particles[0]
+
+    qfactor = p1.qfactor
+    Bfield = p1.Bfield
+    Efield = p1.Efield
+    Volts_to_NU = float(p1.Volts_to_NU)
+
+    profile = {"qfactor": qfactor, "Bfield": Bfield, "Efield": Efield, "Volts_to_NU": Volts_to_NU}
+
+    mu = float(p1.mu)
+    mi = float(p1.mi)
+    qi = float(p1.qi)
+    Pzeta0 = float(p1.Pzeta0)
+
+    constants = {"mu": mu, "mass": mi, "qi": qi, "Pzeta0": Pzeta0}
+
+    for idx, p in enumerate(particles):
+
+        current_P_zeta = p.Pzeta0
+        constants["Pzeta0"] = current_P_zeta
+        psi_wall = p.psi_wall
+
+        current_num_of_fp, current_fp = fixed_points(
+            profile=profile,
+            constants=constants,
+            theta_lim=[theta_min, theta_max],
+            P_theta_lim=[P_theta_min * psi_wall, P_theta_max * psi_wall],
+            iterations=iterations,
+            info=False,
+        )
+
+        current_thetas_fixed = [fixed_point[0] for fixed_point in current_fp]
+        current_P_thetas_fixed = [fixed_point[1] for fixed_point in current_fp]
+
+        if info:
+            print(
+                f"\nCurrent Step: {idx+1} at P_z = {current_P_zeta} with {current_num_of_fp} fixed points"
+            )
+            print(f"Current Fixed Points: {current_fp}\n")
+
+        fp.append(current_fp)
+        num_of_fp.append(current_num_of_fp)
+
+        thetas_fixed.append(current_thetas_fixed)
+        P_thetas_fixed.append(current_P_thetas_fixed)
+
+    return thetas_fixed, P_thetas_fixed, num_of_fp
