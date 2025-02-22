641946bd3eddd63cd3f4cd08a913b2c3b072f243
George <georgetzim23@gmail.com>
2025-02-22 20:59:07 +0200

    added_hessia_as_jacobian

--- gcmotion/utils/single_fixed_point.py
+++ gcmotion/utils/single_fixed_point.py
@@ -3,6 +3,7 @@ Script/function that calculates (a single) fixed point of the GC Hamiltonian, ba
 single initial condition.
 """
 
+from typing import Callable
 import numpy as np
 from scipy.optimize import differential_evolution, fsolve
 from collections import deque
@@ -198,3 +199,81 @@ def _jacobian(theta: float, psi: float, profile: Profile) -> np.ndarray:
     J22 = J22a + J22b
 
     return np.array([[J11, J12], [J21, J22]])
+
+    # def WNU(theta, psi):
+    #     # Calculate the Hamiltonian at (theta, psi)
+    #     psi = max(
+    #         psi, profile.psi_wallNU.m / 100
+    #     )  # Should become small for Pzetas close to 0 because psi-->0
+
+    #     W = profile.findEnergy(
+    #         psi=profile.Q(psi, "NUMagnetic_flux"), theta=theta, units="NUJoule", potential=True
+    #     )
+
+    #     return W.m
+
+    # delta = 1e-5
+    # # Compute the Hessian matrix elements
+    # d2W_dtheta2 = _higher_order_second_derivative(WNU, theta, psi, delta, delta, "x")
+    # d2W_dpsi2 = _higher_order_second_derivative(WNU, theta, psi, delta, delta, "y")
+    # d2W_dtheta_dpsi = _higher_order_second_derivative(WNU, theta, psi, delta, delta, "mixed")
+    # # Hessian matrix
+    # Hessian = np.array([[d2W_dtheta2, d2W_dtheta_dpsi], [d2W_dtheta_dpsi, d2W_dpsi2]])
+
+    # return Hessian
+
+
+# Higher-order central difference for second derivatives
+def _higher_order_second_derivative(
+    f: Callable, x: float, y: float, dx: float, dy: float, respect_to: str
+):
+    r"""
+    Function that numerically calculates the second derivatives (including mixed) of a two
+    variable function.
+
+        Parameters
+        ----------
+        f : Callable
+            Two variable function, whose second order derivatives are to be calculated.
+        x : float
+            x value for which second order derivatives are to be calculated.
+        y : float
+            y value for which second order derivatives are to be calculated.
+        dx : float
+            Finite difference parameter (very small number) used for the calculation of the
+            derivatives with respect to the x variable.
+        dy : float
+            Finite difference parameter (very small number) used for the calculation of the
+            derivatives with respect to the y variable.
+        respect_to : str
+            String that can be either "x" or "y" and indicates with respect to which
+            variable (x or y) will the derivatives be calculated.
+
+
+
+        Returns
+        -------
+        Second order regular or mixed derivative of function f at the selected point (x , y).
+
+
+    """
+    if respect_to == "x":
+        return (
+            -f(x + 2 * dx, y)
+            + 16 * f(x + dx, y)
+            - 30 * f(x, y)
+            + 16 * f(x - dx, y)
+            - f(x - 2 * dx, y)
+        ) / (12 * dx**2)
+    elif respect_to == "y":
+        return (
+            -f(x, y + 2 * dy)
+            + 16 * f(x, y + dy)
+            - 30 * f(x, y)
+            + 16 * f(x, y - dy)
+            - f(x, y - 2 * dy)
+        ) / (12 * dy**2)
+    else:  # Mixed derivative
+        return (f(x + dx, y + dy) - f(x + dx, y - dy) - f(x - dx, y + dy) + f(x - dx, y - dy)) / (
+            4 * dx * dy
+        )
