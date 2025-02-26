f55658cce47759f6a3336e418bf72b9d8990f3e5
George-Tsiamasiotis <gtsiamasiotis@gmail.com>
2025-02-26 22:18:49 +0200

    backup: jit shoelace

---
+++ tests/benchmarks/test_shoelace_benchmark.py
@@ -0,0 +1,34 @@
+import pytest
+import numpy as np
+from numba import njit, guvectorize, float64
+
+
+# @pytest.fixture(scope="module")
+def shoelace(x, y) -> float:
+    return float(
+        0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+    )
+
+
+# @pytest.fixture(scope="module")
+@njit("float64(float64[::1], float64[::1])")
+def shoelace_jit(x, y) -> float:
+    return float(
+        0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+    )
+
+
+x, y = (*np.random.random((2, 1000)),)
+
+# Call it once to compile
+_ = shoelace_jit(x=x, y=y)
+
+
+def test_shoelace(benchmark):
+    result = benchmark(shoelace, x=x, y=y)
+    assert True
+
+
+def test_shoelace_jit(benchmark):
+    result = benchmark(shoelace_jit, x=x, y=y)
+    assert True
