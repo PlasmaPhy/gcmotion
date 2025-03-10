51e97b2091652b20dbfddf774df0f48cea65b101
George-Tsiamasiotis <gtsiamasiotis@gmail.com>
2025-03-10 22:49:23 +0200

    maint: Changed magnetic_profile cmap to managua!
    maint: Chained the god awful blue figure background color to white

--- gcmotion/configuration/plot_parameters.py
+++ gcmotion/configuration/plot_parameters.py
@@ -3,6 +3,7 @@ from numpy import pi
 
 figsize = 13, 7  # Global window size
 dpi = 100  # Global dpi
+facecolor = "white"  # Global figure background color
 
 
 @dataclass()
@@ -11,7 +12,7 @@ class ProfileEnergyContourConfig:
     figsize: tuple = figsize
     dpi: int = dpi
     layout: str = "constrained"
-    facecolor: str = "lightskyblue"
+    facecolor: str = facecolor
     projection: str | None = None  # None = default
     # Default parameter values
     thetalim: tuple = (-pi, pi)
@@ -35,7 +36,7 @@ class ProfilePzetaContourConfig:
     figsize: tuple = figsize
     dpi: int = dpi
     layout: str = "constrained"
-    facecolor: str = "lightskyblue"
+    facecolor: str = facecolor
     # Default parameter values
     zetalim: tuple = (-pi, pi)
     psilim: tuple = (0, 1.2)  # times psi_wall
@@ -57,7 +58,7 @@ class QfactorProfileConfig:
     figsize: tuple = (13, 5)
     dpi: int = dpi
     layout: str = "constrained"
-    facecolor: str = "lightskyblue"
+    facecolor: str = facecolor
     titlesize: float = 20
     titlecolor: str = "blue"
     # Default parameter values
@@ -80,7 +81,7 @@ class MagneticProfileConfig:
     figsize: tuple = (13, 7)
     dpi: int = dpi
     layout: str = "constrained"
-    facecolor: str = "lightskyblue"
+    facecolor: str = facecolor
     titlesize: float = 20
     titlecolor: str = "blue"
     # Default parameter values
@@ -92,9 +93,9 @@ class MagneticProfileConfig:
     # Contour options
     grid_density: int = 200
     levels: int = 20
-    bcmap: str = "inferno"
-    icmap: str = "viridis"
-    gcmap: str = "viridis"
+    bcmap: str = "managua"
+    icmap: str = "managua"
+    gcmap: str = "managua"
     locator: str = ""
     log_base: float = 1.00001
     # 2d plot options
@@ -113,7 +114,7 @@ class ParticleEvolutionConfig:
     figsize: tuple = figsize
     dpi: int = dpi
     layout: str = "constrained"
-    facecolor: str = "lightskyblue"
+    facecolor: str = facecolor
     titlesize: float = 20
     titlecolor: str = "blue"
     # Default parameter values
@@ -135,7 +136,7 @@ class ParticlePoloidalDrift:
     figsize: tuple = figsize
     dpi: int = dpi
     layout: str = "constrained"
-    facecolor: str = "lightskyblue"
+    facecolor: str = facecolor
     # Default parameter values
     projection: str | None = None  # None = default
     thetalim: tuple = (-pi, pi)
